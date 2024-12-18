b8c561487006644cee4dcdea1830c60f8334280f
Siraj Abdulkadir <imsirajabdulkadir@gmail.com>
2024-12-18 16:20:06 +0300

    v1 works properly for prime number upto 701 and any composite numbers

--- primeNumber.py
+++ primeNumber.py
@@ -1,5 +1,5 @@
 myInput = int(input("Enter any number:"))
-primeNumbers = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
+primeNumbers = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103]
 
 if(myInput < 1):
  print("The number have to be greater than 1!")
